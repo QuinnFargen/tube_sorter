ee15318220683a95320cf593143eb5ad88588f19
Quinn Fargen <quinnfargen@gmail.com>
2023-02-21 00:13:43 -0600

    working tubesorter, stuck 4x14

--- TubeSorter.py
+++ TubeSorter.py
@@ -1,7 +1,8 @@
 
 import Ex_tubes
-from collections import Counter
-import time
+# from collections import Counter
+# import time
+import random
 
 
 class TubeSorter:
@@ -13,27 +14,41 @@ class TubeSorter:
         self.balls = self.ball_unique()
         self.movelog = []
         self.tops = self.tubes_tops()
+        self.cols = self.tubes_avail()
+        # ADD LATER: reset feature if deadend, counter to limit resets
 
     def start_removeZero(self, m):
         for i in range(self.width):
-            if self.tube_num(m[i]) < self.heigth:
+            if self.start_tubenum(m[i]) < self.heigth:
                 m[i] = [z for z in m[i] if z != 0]
         return m
+
+    def start_tubenum(self, t):      # Return # balls
+        return sum(map(lambda x : x != 0, t))   #used to remove zero, so not blank yet
 
     def tube_check(self, tn):    # Check if tube good, all match or empty
         if self.tubes[tn] == []:
             return True
-        else:
+        elif len(self.tubes[tn]) == self.heigth: # Full tube to all match
             return all(elem == self.tubes[tn][0] for elem in self.tubes[tn])
-    
+        else:
+            False
+
     def tubes_check(self):      # Check all tubes
         for t in range(self.width):
             if not self.tube_check(tn = t):     # if one tube fails, False
                 return False
         return True
     
-    def tube_num(self, t):      # Return # balls
-        return sum(map(lambda x : x != 0, t))
+
+    def tubes_avail(self):
+        avail = []
+        for i in range(self.width):
+            if self.tubes[i] == []:
+                avail.append(i)
+            elif not self.tube_check(tn = i):
+                avail.append(i)
+        return avail
 
     def tube_top(self, t):      # Return top ball
         for i in reversed(range(len(t) - 3)):
@@ -53,7 +68,7 @@ class TubeSorter:
     def move_check(self, t1,t2):        # Check if can move 1 from t1 to t2
         tube1 = self.tubes[t1]
         tube2 = self.tubes[t2]
-        if self.tube_num(tube2) == 4:      #full tube
+        if len(tube2) == 4:      #full tube
             return False
         elif tube2[0] == 0 :               #empty tube
             return True
@@ -64,10 +79,15 @@ class TubeSorter:
     def tubes_tops(self):
         tops = []
         for i in range(self.width):
-            if self.tube_num(self.tubes[i]) > 0:
-                ball = list(Counter(self.tubes[i]).keys())[-1]
-                num = list(Counter(self.tubes[i]).values())[-1]
-                avl = self.heigth - self.tube_num(self.tubes[i])
+            if len(self.tubes[i]) > 0:
+                ball = self.tubes[i][-1]
+                num = 0
+                for b in reversed(self.tubes[i]):
+                    if b == ball:
+                        num += 1
+                    else:
+                        break
+                avl = self.heigth - len(self.tubes[i])
                 tops.append([ball,num,avl])   
             else:
                 tops.append([0,4,4]) # Empty Row
@@ -77,27 +97,32 @@ class TubeSorter:
             # 1 check oposite move wasn't just performed
             # Shouldn't be able to move back though with check of them not matching #2
             # ADD LATER: more extensive check on multi recent that there isn't an endless loop of backtracking
-        if self.movelog != []:  # Don't check on first move
-            if self.movelog[-1] == [t2,t1]:
-                # return False
-                print("Opp Move Canceled")
-            # 2 check top balls match or is empty in t2
+        lastMove = []
+        if self.movelog != []: 
+            lastMove = self.movelog[-1] 
+
+        if lastMove == [t2,t1]:  # Don't check on first move
+            # print("Opp Move Canceled")
+            return False
+            # 2 check top balls match and is empty in t2
         elif self.tops[t1][0] != self.tops[t2][0] and self.tops[t2][0] != 0:
-                # return False
-                print("Top Balls don't match")
-            # 3 check num of ball t1 fits in t2
-        elif self.tops[t1][1] > self.tops[t2][2]:
-                # return False
-                print("Num Ball Dont Fit")
-            # 4 remove balls from t1, add to t2, log move, update tops
+        # elif self.tops[t1][0] != self.tops[t2][0] or (self.tops[t1] == [] and self.tops[t2] == []):
+            # print("Top Balls don't match")
+            return False
+            # 3 check num of ball t1 fits in t2 or moving empty
+        elif self.tops[t1][1] > self.tops[t2][2] or self.tops[t1][0] == 0:
+            # print("Num Ball Dont Fit")
+            return False
+            # 4 remove balls from t1, add to t2, log move, update tops & cols
         else:
             b1 = self.tops[t1][0]
             n1 = self.tops[t1][1]
-            self.tubes[t1] = [z for z in self.tubes[t1] if z != b1]
+            self.tubes[t1] = self.tubes[t1][: len(self.tubes[t1]) - n1]
             for n in range(n1):
                 self.tubes[t2].append(b1)
             self.movelog.append([t1,t2])
             self.tops = self.tubes_tops()
+            self.cols = self.tubes_avail()
             return True
 
     def solve_check(self):           # Check solvable still or solved
@@ -109,41 +134,40 @@ class TubeSorter:
         return False
 
     def Sorter(self):
-        i = 0
-        while not self.solve_check() and len(self.movelog) < 50 and i < 2000:
-            for f in range(self.width):
-                for s in range(self.width):
-                    print([f,s])
-                    time.sleep(1)
-                    self.move_top(t1 = f, t2 = s)
-                    print(self.tops)
-                    i += 1
+        i = 0; found = 0
+        while not self.solve_check() and len(self.movelog) < 100 and i < 2000:
+            random.shuffle(self.cols)
+            for f in self.cols:
+                if found == 1:
+                    found  = 0; break # escape for F
+                for s in self.cols:
+                    if f != s:  # don't move to same column
+                        i += 1
+                        # time.sleep(1); print([f,s])
+                        if self.move_top(t1 = f, t2 = s):
+                            # print(self.tubes); print(self.tops); print(self.cols)
+                            found = 1; break # escape for S
+                            # print(self.tops)
                     # print(self.movelog)
+        print(self.tubes)
                 
 
 
 Ex_tubes.t_4x6_1234
 Ex_tubes.t_4x6_easy
 Ex_tubes.t_4x6_solved
+Ex_tubes.t_4x14_hard
 
 
-plz = TubeSorter(matrix = Ex_tubes.t_4x6_easy)
+plz = TubeSorter(matrix = Ex_tubes.t_4x14_hard)
 plz.tubes
 plz.tops
-plz.move_top(0,4)
-plz.move_top(2,4)
-plz.move_top(3,4)
+plz.cols
 plz.movelog
-plz.movelog[-1]
 
-plz.solve_check()
-plz.tube_check(t=0)
 plz.Sorter()
 
-plz.tubes_tally()
-
-
-
+plz.move_top(t1=4,t2=5)
 
 
 
